eab2794dbaa10f65afcbb42b0cd8fe43c76a8fd2
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-03-06 09:06:02 +0800

    Move win fun added

--- src/main/kotlin/Field.kt
+++ src/main/kotlin/Field.kt
@@ -32,5 +32,68 @@ class Field(private val size: Int) {
         }
     }
 
-    fun isPositionValid(x: Int, y: Int) = ((x in 0 until size) && (y in 0 until size))
+
+    fun isMoveWin(x: Int, y: Int, figure: String): Boolean {
+        return when {
+            checkColumn(x, y, figure) -> true
+            checkRow(x, y, figure) -> true
+            checkDiagonalFirst(x, y, figure) -> true
+            checkDiagonalSecond(x, y, figure) -> true
+            else -> false
+        }
+    }
+
+    fun resetGame() {
+        resetField()
+        isGameFinished = false
+        moveCount = 0
+    }
+
+    private fun isPositionValid(x: Int, y: Int) = ((x in 0 until size) && (y in 0 until size))
+
+    private fun checkColumn(x: Int, y: Int, figure: String): Boolean {
+        for (i in 0 until size) {
+            when {
+                field[x][i] != figure -> break
+                (i == size - 1) -> return true
+            }
+
+        }
+        return false
+    }
+
+    private fun checkRow(x: Int, y: Int, figure: String): Boolean {
+        for (i in 0 until size) {
+            when {
+                field[i][y] != figure -> break
+                (i == size - 1) -> return true
+            }
+        }
+        return false
+    }
+
+    private fun checkDiagonalFirst(x: Int, y: Int, figure: String): Boolean {
+        if (x == y) {
+            for (i in 0 until size) {
+                when {
+                    field[i][i] != figure -> break
+                    (i == size - 1) -> return true
+                }
+            }
+
+        }
+        return false
+    }
+
+    private fun checkDiagonalSecond(x: Int, y: Int, figure: String): Boolean {
+        if (x + y == size - 1) {
+            for (i in 0 until size) {
+                when {
+                    field[i][(size - 1 - i)] != figure -> break
+                    (i == size - 1) -> return true
+                }
+            }
+        }
+        return false
+    }
 }
